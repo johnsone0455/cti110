a64e3e6de5cd48e5d32d193d8d5c11324bd6537a
johnsone0455 <55255065+johnsone0455@users.noreply.github.com>
2019-09-17 13:58:01 -0400

    Add files via upload
    Python code of turtle drawing

---
+++ P2HW2_TurtleGraphic_ElizabethJohnson.py
@@ -0,0 +1,55 @@
+# Python code of turtle command drawing
+# September 12 2019
+# CTI-110 P2HW2 - Turtle Graphic
+# Elizabeth Johnson
+
+import turtle
+turtle.bgcolor('medium turquoise')
+turtle.pencolor('black')
+turtle.width('5')
+turtle.fillcolor('white')
+turtle.left(45)
+turtle.begin_fill()
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+turtle.end_fill()
+turtle.begin_fill()
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.end_fill()
+
+# Turtle will draw the first picture option of the two diamonds with a filled color
+# Import turtle to begin the drawing process
+# Changing the background color in order to see the filling of the shapes
+# Adjusting the pen color and size
+# Changing the color the shapes will fill with
+# Turning turtle to make the diamond shape
+# Commanding turtle to fill the shape that is being created
+# Sending turtle forward to make the first side
+# Turning turtle to continue making the shape
+# Sending turtle forward to create another side
+# Turning turtle to make the 3rd side
+# Moving turtle forward to create another side
+# Turning turtle the last time for the first shape
+# Drawing the last side of this first diamond
+# Commanding turtle to fill the shape that has just been finished being created
+# Now beginning to make the second shape
+# Commanding to turtle to fill this new shape once it is done being created
+# Creating the first side of the second shape
+# Now turning turtle to the left to continue the shape
+# Turtle will now create the second side of the second diamond
+# Turning turtle to create another side
+# Turtle moves forward to create the third side of the diamond
+# Turning turtle the last time to create the final side
+# Turtle will now draw the final side for the diamond
+# Commanding turtle to fill this new shape that has just been made
